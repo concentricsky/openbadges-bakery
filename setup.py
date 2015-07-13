7d00d2fd3bd67571fb4731c544072d3ef008958c
Nate Otto <nate@ottonomy.net>
2015-07-13 14:36:02 -0700

    v0.1.4 update django dependency to be less strict

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='openbadges_bakery',
-    version='0.1.3',
+    version='0.1.4',
     packages=['openbadges_bakery'],
     include_package_data=True,
     license='aGPL License',
@@ -34,6 +34,6 @@ setup(
         'Intended Audience :: Developers'
     ],
     install_requires=[
-        'django == 1.7.1'
+        'django >= 1.7'
     ],
 )
