13700b7ecff6c71b3a99e0246f15738f0d2871d6
Nate Otto <nate@ottonomy.net>
2015-07-13 14:27:09 -0700

    v0.1.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='openbadges_bakery',
-    version='0.1.1',
+    version='0.1.3',
     packages=['openbadges_bakery'],
     include_package_data=True,
     license='aGPL License',
