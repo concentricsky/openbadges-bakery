b944eeb173a70d8b10dbd01d2e7ebebc6fa41127
Nate Otto <nate@ottonomy.net>
2020-11-17 17:16:00 -0800

    v1.2.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='openbadges_bakery',
-    version='1.2.1',
+    version='1.2.2',
     packages=['openbadges_bakery'],
     include_package_data=True,
     license='Apache License 2.0',
