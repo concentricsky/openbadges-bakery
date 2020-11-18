f7ed2aa13f53ccd17e005020966fe806a541f75d
Nate Otto <nate@ottonomy.net>
2020-11-17 17:15:35 -0800

    Python 3 support nominally declared

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Education',
         'Topic :: Utilities',
